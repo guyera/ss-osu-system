4fa9e647742fe57f7ead2387548682b311b9ef78
anubhav <anubhav@umd.edu>
2021-08-18 17:30:07 +0530

    bugfixes in test script

--- test.py
+++ test.py
@@ -1,23 +1,42 @@
 import os
 import torch
 import argparse
-import torchvision
 import torch.distributed as dist
 import torch.multiprocessing as mp
 from torch.utils.data import DataLoader, DistributedSampler
 
-import pickle
 import pocket
 import numpy as np
+from tqdm import tqdm
 
 from models.scg import SpatiallyConditionedGraph as SCG
 from data.data_factory import DataFactory, CustomInput
 from utils import custom_collate
 
 
+def get_net(args):
+    nets = {
+        'scg': SCG(
+                args.object_to_target, args.human_idx, num_classes=args.num_classes,
+                num_obj_classes=args.num_obj_classes,
+                num_iterations=args.num_iter, postprocess=False,
+                max_human=args.max_human, max_object=args.max_object,
+                box_score_thresh=args.box_score_thresh,
+                distributed=True
+            ),
+        'idn': '',
+        'drg': '',
+        'cascaded-hoi': '',
+    }
+    if args.net not in nets:
+        raise NotImplementedError
+    return nets[args.net]
+
+
 class Test(object):
-    def __init__(self, net, model_name):
+    def __init__(self, net, model_name, data_loader):
         self.net = net
+        self.data_loader = data_loader
         self.func_map = {
             'scg': self.scg,
             'drg': self.drg,
@@ -25,28 +44,42 @@ class Test(object):
             'cascaded-hoi': self.cascaded_hoi,
         }
         self.test = self.func_map[model_name]
+        self.converter = CustomInput(model_name).converter
 
-    def scg(self, input_data, ov_interaction_map):
-        input_data = pocket.ops.relocate_to_cuda(input_data)
-        with torch.no_grad():
-            output = self.net(*input_data)
-            # Batch size is fixed as 1 for inference
-            assert len(output) == 1, "Batch size is not 1"
-            output = pocket.ops.relocate_to_cpu(output[0])
-            # Format detections
-            box_idx = output['index']
-            # boxes_h = output['boxes_h'][box_idx]
-            # boxes_o = output['boxes_o'][box_idx]
-            objects = output['object'][box_idx]
-            scores = output['scores']
-            verbs = output['prediction']
-            interactions = torch.tensor([
-                ov_interaction_map[o][v]
-                for o, v in zip(objects, verbs)
-            ])
-            # Associate detected pairs with ground truth pairs
-            labels = torch.zeros_like(scores)
-        return scores, interactions, labels
+    def scg(self, ov_interaction_map):
+        results = list()
+        for batch in tqdm(self.data_loader):
+            inputs = pocket.ops.relocate_to_cuda(batch[:-1])
+            # TODO: Optimize this
+            inp_image = np.array(inputs[0][0].cpu())
+            inp_boxes = np.array(inputs[1][0]['boxes'].cpu())
+            inp_labels = np.array(inputs[1][0]['labels'].cpu())
+            inp_scores = np.array(inputs[1][0]['scores'].cpu())
+            input_data = self.converter(inp_image, inp_boxes, inp_labels, inp_scores)
+            input_data = pocket.ops.relocate_to_cuda(input_data)
+            with torch.no_grad():
+                output = self.net(*input_data)
+                # Batch size is fixed as 1 for inference
+                assert len(output) == 1, "Batch size is not 1"
+                output = pocket.ops.relocate_to_cpu(output[0])
+                # Format detections
+                box_idx = output['index']
+                objects = output['object'][box_idx]
+                scores = output['scores']
+                verbs = output['prediction']
+                interactions = torch.tensor([
+                    ov_interaction_map[o][v]
+                    for o, v in zip(objects, verbs)
+                ])
+                # Associate detected pairs with ground truth pairs
+                labels = torch.zeros_like(scores)
+                result = {
+                    'scores': scores,
+                    'interactions': interactions,
+                    'labels': labels,
+                }
+                results.append(result)
+        return results
 
     def drg(self, input_data):
         raise NotImplementedError
@@ -84,30 +117,21 @@ def main(rank, args):
             rank=rank)
     )
     if args.dataset == 'hicodet':
-        object_to_target = val_loader.dataset.dataset.object_to_verb
-        human_idx = 49
-        num_classes = 117
+        args.object_to_target = val_loader.dataset.dataset.object_to_verb
+        args.human_idx = 49
+        args.num_classes = 117
     elif args.dataset == 'vcoco':
-        object_to_target = val_loader.dataset.dataset.object_to_action
-        human_idx = 1
-        num_classes = 24
+        args.object_to_target = val_loader.dataset.dataset.object_to_action
+        args.human_idx = 1
+        args.num_classes = 24
+    args.num_obj_classes = val_loader.dataset.dataset.num_object_cls
+    net = get_net(args)
+    if net == '':
+        raise NotImplementedError
 
-    # num_anno = torch.tensor(HICODet(None, anno_file=os.path.join(
-    #     args.data_root, 'instances_train2015.json')).anno_interaction)
-    # rare = torch.nonzero(num_anno < 10).squeeze(1)
-    # non_rare = torch.nonzero(num_anno >= 10).squeeze(1)
-
-    net = SCG(
-        object_to_target, human_idx, num_classes=num_classes,
-        num_iterations=args.num_iter, postprocess=False,
-        max_human=args.max_human, max_object=args.max_object,
-        box_score_thresh=args.box_score_thresh,
-        distributed=True
-    )
-
-    if os.path.exists(args.model_path):
-        print("Loading model from ", args.model_path)
-        checkpoint = torch.load(args.model_path, map_location="cpu")
+    if os.path.exists(args.checkpoint_path):
+        print("Loading model from ", args.checkpoint_path)
+        checkpoint = torch.load(args.checkpoint_path, map_location="cpu")
         net.load_state_dict(checkpoint['model_state_dict'])
     elif len(args.model_path):
         print("\nWARNING: The given model path does not exist. "
@@ -115,26 +139,11 @@ def main(rank, args):
 
     net.cuda()
     net.eval()
-    # Sample input test
-    test_input = pickle.load(open('inputs.pkl', 'rb'))
-    image = np.array(test_input[0][0].cpu())
-    boxes = np.array(test_input[1][0]['boxes'].cpu())
-    labels = np.array(test_input[1][0]['labels'].cpu())
-    scores = np.array(test_input[1][0]['scores'].cpu())
-    # TODO: Pass model_name through args here, also implement conditional calling based on models
-    tester = Test(net, 'scg').test
-    converter = CustomInput('scg').converter
-    input_data = converter(image, boxes, labels, scores)
-    tester(input_data, val_loader.dataset.dataset.object_n_verb_to_interaction)
-    # timer = pocket.utils.HandyTimer(maxlen=1)
-
-    # with timer:
-    #     test_ap = test(net, dataloader)
-    # print("Model at epoch: {} | time elapsed: {:.2f}s\n"
-    #       "Full: {:.4f}, rare: {:.4f}, non-rare: {:.4f}".format(
-    #     epoch, timer[0], test_ap.mean(),
-    #     test_ap[rare].mean(), test_ap[non_rare].mean()
-    # ))
+    tester = Test(net, args.net, val_loader).test
+    if args.net == 'scg':
+        tester(val_loader.dataset.dataset.object_n_verb_to_interaction)
+    else:
+        raise NotImplementedError
 
 
 if __name__ == "__main__":
@@ -142,6 +151,7 @@ if __name__ == "__main__":
     parser.add_argument('--world-size', required=True, type=int,
                         help="Number of subprocesses/GPUs to use")
     parser.add_argument('--dataset', default='hicodet', type=str)
+    parser.add_argument('--net', default='scg', type=str)
     parser.add_argument('--partitions', nargs='+', default=['train2015', 'test2015'], type=str)
     parser.add_argument('--data-root', default='hicodet', type=str)
     parser.add_argument('--detection-dir', default='hicodet/detections/test2015',
@@ -153,7 +163,7 @@ if __name__ == "__main__":
     parser.add_argument('--max-human', default=15, type=int)
     parser.add_argument('--max-object', default=15, type=int)
     parser.add_argument('--num-workers', default=2, type=int)
-    parser.add_argument('--model-path', default='', type=str)
+    parser.add_argument('--checkpoint-path', default='', type=str)
     parser.add_argument('--batch-size', default=1, type=int,
                         help="Batch size for each subprocess")
 
