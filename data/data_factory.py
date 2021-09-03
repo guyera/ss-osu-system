324c5a18fb83a92a1d0640afdd5a47f9bf215571
anubhav <anubhav@umd.edu>
2021-09-03 08:45:17 -0400

    bugfix

--- data/data_factory.py
+++ data/data_factory.py
@@ -169,10 +169,13 @@ class DataFactory(Dataset):
         object_boxes = boxes[object_idxs].view(-1, 4)
         object_scores = scores[object_idxs].view(-1)
         object_labels = labels[object_idxs].view(-1)
-
-        return dict(subject_boxes=subject_boxes, subject_labels=subject_labels, subject_scores=subject_scores,
-                    object_boxes=object_boxes, object_labels=object_labels, object_scores=object_scores,
-                    img_path=detection['img_path'])
+        if self.training:
+            return dict(subject_boxes=subject_boxes, subject_labels=subject_labels, subject_scores=subject_scores,
+                        object_boxes=object_boxes, object_labels=object_labels, object_scores=object_scores)
+        else:
+            return dict(subject_boxes=subject_boxes, subject_labels=subject_labels, subject_scores=subject_scores,
+                        object_boxes=object_boxes, object_labels=object_labels, object_scores=object_scores,
+                        img_path=detection['img_path'])
 
     def flip_boxes(self, detection, target, w):
         detection['boxes'] = pocket.ops.horizontal_flip_boxes(w, detection['boxes'])
