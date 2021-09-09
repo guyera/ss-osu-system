e63eb2c6b3e239924dfc14ba7b07eac1982c8003
Sonaal Kant <sonaalk@cmlsub00.umiacs.umd.edu>
2021-09-08 20:46:40 -0400

    responding to reviews

--- data/custom.py
+++ data/custom.py
@@ -40,46 +40,6 @@ class StandardTransform:
 
         return '\n'.join(body)
 
-class CustomDetSubset(DataSubset):
-    def __init__(self, *args) -> None:
-        super().__init__(*args)
-
-    def filename(self, idx: int) -> str:
-        """Override: return the image file name in the subset"""
-        return self._filenames[self._idx[self.pool[idx]]]
-
-    def image_size(self, idx: int) -> Tuple[int, int]:
-        """Override: return the size (width, height) of an image in the subset"""
-        return self._image_sizes[self._idx[self.pool[idx]]]
-
-    @property
-    def anno_interaction(self) -> List[int]:
-        """Override: Number of annotated box pairs for each interaction class"""
-        num_anno = [0 for _ in range(self.num_interation_cls)]
-        intra_idx = [self._idx[i] for i in self.pool]
-        for idx in intra_idx:
-            for hoi in self._anno[idx]['hoi']:
-                num_anno[hoi] += 1
-        return num_anno
-
-    @property
-    def anno_object(self) -> List[int]:
-        """Override: Number of annotated box pairs for each object class"""
-        num_anno = [0 for _ in range(self.num_object_cls)]
-        anno_interaction = self.anno_interaction
-        for corr in self._class_corr:
-            num_anno[corr[1]] += anno_interaction[corr[0]]
-        return num_anno
-
-    @property
-    def anno_action(self) -> List[int]:
-        """Override: Number of annotated box pairs for each action class"""
-        num_anno = [0 for _ in range(self.num_action_cls)]
-        anno_interaction = self.anno_interaction
-        for corr in self._class_corr:
-            num_anno[corr[2]] += anno_interaction[corr[0]]
-        return num_anno
-
 
 class CustomDet(Dataset):
     """
@@ -152,8 +112,6 @@ class CustomDet(Dataset):
     def __repr__(self) -> str:
         """Return the executable string representation"""
         reprstr = self.__class__.__name__ + '(csvPath=' + repr(self._root)
-        # reprstr += ', anno_file='
-        # reprstr += repr(self._anno_file)
         reprstr += ')'
         # Ignore the optional arguments
         return reprstr
@@ -162,105 +120,11 @@ class CustomDet(Dataset):
         """Return the readable string representation"""
         reprstr = 'Dataset: ' + self.__class__.__name__ + '\n'
         reprstr += '\tNumber of images: {}\n'.format(self.__len__())
-        # reprstr += '\tImage directory: {}\n'.format(self._root)
-        # reprstr += '\tAnnotation file: {}\n'.format(self._root)
         return reprstr
 
     @property
     def annotations(self) -> List[dict]:
         return self._anno
-
-    @property
-    def class_corr(self) -> List[Tuple[int, int, int]]:
-        """
-        Class correspondence matrix in zero-based index
-        [
-            [hoi_idx, obj_idx, verb_idx],
-            ...
-        ]
-
-        Returns:
-            list[list[3]]
-        """
-        return self._class_corr.copy()
-
-    @property
-    def object_n_verb_to_interaction(self) -> List[list]:
-        """
-        The interaction classes corresponding to an object-verb pair
-
-        CustomDet.object_n_verb_to_interaction[obj_idx][verb_idx] gives interaction class
-        index if the pair is valid, None otherwise
-
-        Returns:
-            list[list[117]]
-        """
-        lut = np.full([self.num_object_cls, self.num_action_cls], None)
-        for i, j, k in self._class_corr:
-            lut[j, k] = i
-        return lut.tolist()
-
-    @property
-    def object_to_interaction(self) -> List[list]:
-        """
-        The interaction classes that involve each object type
-
-        Returns:
-            list[list]
-        """
-        obj_to_int = [[] for _ in range(self.num_object_cls)]
-        for corr in self._class_corr:
-            obj_to_int[corr[1]].append(corr[0])
-        return obj_to_int
-
-    @property
-    def object_to_verb(self) -> List[list]:
-        """
-        The valid verbs for each object type
-
-        Returns:
-            list[list]
-        """
-        obj_to_verb = [[] for _ in range(self.num_object_cls)]
-        for corr in self._class_corr:
-            obj_to_verb[corr[1]].append(corr[2])
-        return obj_to_verb
-
-    @property
-    def anno_interaction(self) -> List[int]:
-        """
-        Number of annotated box pairs for each interaction class
-
-        Returns:
-            list[600]
-        """
-        return self._num_anno.copy()
-
-    @property
-    def anno_object(self) -> List[int]:
-        """
-        Number of annotated box pairs for each object class
-
-        Returns:
-            list[80]
-        """
-        num_anno = [0 for _ in range(self.num_object_cls)]
-        for corr in self._class_corr:
-            num_anno[corr[1]] += self._num_anno[corr[0]]
-        return num_anno
-
-    @property
-    def anno_action(self) -> List[int]:
-        """
-        Number of annotated box pairs for each action class
-
-        Returns:
-            list[117]
-        """
-        num_anno = [0 for _ in range(self.num_action_cls)]
-        for corr in self._class_corr:
-            num_anno[corr[2]] += self._num_anno[corr[0]]
-        return num_anno
 
     @property
     def objects(self) -> List[str]:
@@ -282,30 +146,6 @@ class CustomDet(Dataset):
         """
         return self._verbs.copy()
 
-    @property
-    def interactions(self) -> List[str]:
-        """
-        Combination of verbs and objects
-
-        Returns:
-            list[str]
-        """
-        return [self._verbs[j] + ' ' + self.objects[i]
-                for _, i, j in self._class_corr]
-
-    def split(self, ratio: float) -> Tuple[CustomDetSubset, CustomDetSubset]:
-        """
-        Split the dataset according to given ratio
-
-        Arguments:
-            ratio(float): The percentage of training set between 0 and 1
-        Returns:
-            train(Dataset)
-            val(Dataset)
-        """
-        perm = np.random.permutation(len(self._idx))
-        n = int(len(perm) * ratio)
-        return CustomDetSubset(self, perm[:n]), CustomDetSubset(self, perm[n:])
 
     def filename(self, idx: int) -> str:
         """Return the image file name given the index"""
@@ -330,7 +170,6 @@ class CustomDet(Dataset):
 
         self._anno = self.create_annotation(df, self._filenames)
         
-        self._class_corr = self.create_correspondence(df, self._objects, self._verbs)
         self._image_sizes = self.create_sizes(df)
 
         self.num_object_cls = len(self._objects)
@@ -343,6 +182,7 @@ class CustomDet(Dataset):
         self._idx = idx
 
     def create_annotation(self, df, imnames):
+        # TODO : Make This Faster
         annots = list()
         for imname in imnames:
             annot = dict()
