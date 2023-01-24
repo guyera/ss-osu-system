4bc68b5c387f8a471f3655029e86e3d613161e15
Alexander Guyer <guyera@oregonstate.edu>
2023-01-24 14:33:31 -0800

    Add side tuning interface for computing backbone and side network features separately

--- sidetuningbackbone.py
+++ sidetuningbackbone.py
@@ -5,7 +5,9 @@ class SideTuningBackbone(Module):
         super().__init__()
         self._backbone = backbone
         self.device = backbone.device
-        self.reset()
+        # The swin_et is intially None and set only upon the first backbone
+        # reset, which should occur just before the first retraining
+        self._model = None
 
     def reset(self):
         # This does NOT reset the underlying backbone, since the backbone should
@@ -14,15 +16,22 @@ class SideTuningBackbone(Module):
         self._model = swin_et(num_classes=256).to(self.device)
 
     def forward(self, x):
-        y1 = self._backbone(x)
-        y2 = self._model(x)
-        x = torch.cat((y1, y2), dim=-1)
+        # If retraining has occurred at least once, then the features are a
+        # concatenation between the backbone's features and the side network's
+        # features. Otherwise, they're just the backbone's features
+        if self._model is not None:
+            y1 = self._backbone(x)
+            y2 = self._model(x)
+            x = torch.cat((y1, y2), dim=-1)
+        else:
+            x = self._backbone(x)
         return x
 
     def to(self, device):
         super().to(device)
         self._backbone = self._backbone.to(device)
-        self._model = self._model.to(device)
+        if self._model is not None:
+            self._model = self._model.to(device)
         self.device = device
         return self
 
@@ -33,3 +42,20 @@ class SideTuningBackbone(Module):
         # Backbone parameters are not retrainable; only return swin_et's
         # parameters
         return self._model.parameters()
+
+    '''
+    Computes the features just from the backbone---not the side network.
+    Useful when training only the side network, leaving the backbone frozen.
+    '''
+    def compute_backbone_features(self, x):
+        return self._backbone(x)
+
+    '''
+    Computes the features just from the side network---not the backbone.
+    Useful when training only the side network, leaving the backbone frozen.
+    '''
+    def compute_side_features(self, x):
+        if self._model is None:
+            return RuntimeError(('Cannot compute side features before '
+                                 'resetting model for retraining at least once'
+                                 '---side network not initialized'))
