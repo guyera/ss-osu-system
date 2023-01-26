0c0b0227ac75fbb0c18bae597eeb1099a27532f1
Alexander Guyer <guyera@oregonstate.edu>
2023-01-26 15:57:31 -0800

    Update top level to reflect module interface changes

--- toplevel/util.py
+++ toplevel/util.py
@@ -1,11 +1,10 @@
 import torch
-from tupleprediction import\
-    Case1LogisticRegression,\
-    Case2LogisticRegression,\
-    Case3LogisticRegression,\
-    TuplePredictor
-from scoring import ActivationStatisticalModel
-from boxclassifier import ClassifierV2
+from tupleprediction import NoveltyTypeClassifier, TuplePredictor
+from scoring import\
+    ActivationStatisticalModel,\
+    make_logit_scorer,\
+    CompositeScorer
+from boxclassifier import ClassifierV2, ConfidenceCalibrator
 
 import os
 
@@ -56,120 +55,115 @@ class UnsupervisedNoveltyDetectionManager:
     def __init__(self, 
         model_dir,
         backbone_architecture,
-        num_subject_classes, 
-        num_verb_classes,
-        num_object_classes, 
-        num_spatial_features,
+        n_species_cls,
+        n_activity_cls,
+        n_known_species_cls,
+        n_known_activity_cls,
         p_type_alpha):
        
         self.p_type_alpha = p_type_alpha
 
-        self.classifier = ClassifierV2(256, num_subject_classes, num_object_classes, num_verb_classes, num_spatial_features)
-        self.tuple_predictor = TuplePredictor(num_subject_classes, num_object_classes, num_verb_classes)
-        
+        self.tuple_predictor = TuplePredictor(
+            n_known_species_cls,
+            n_known_activity_cls
+        ).to('cuda:0')
+
         pretrained_path = os.path.join(
             model_dir,
             backbone_architecture.value['name']
         )
-        classifier_state_dict = torch.load(os.path.join(
-            pretrained_path,
-            'classifier.pth'
-        ))
-        tuple_prediction_state_dict = torch.load(os.path.join(
-            pretrained_path,
-            'tuple-prediction.pth'
-        ))
+
+        classifier_state_dict = torch.load(
+            os.path.join(
+                pretrained_path,
+                'classifier.pth'
+            ),
+            map_location='cuda:0'
+        )
+        self.classifier = ClassifierV2(
+            256,
+            n_species_cls,
+            n_activity_cls
+        ).to('cuda:0')
         self.classifier.load_state_dict(classifier_state_dict)
-        self.classifier = self.classifier.to('cuda:0')
-        self.tuple_predictor.load_state_dict(tuple_prediction_state_dict)
-        self.tuple_predictor = self.tuple_predictor.to('cuda:0')
-        self.activation_statistical_model = ActivationStatisticalModel(backbone_architecture).to('cuda:0')
-        self.activation_statistical_model.load_state_dict(tuple_prediction_state_dict['activation_statistical_model'])
-        
-        self.case_1_logistic_regression = Case1LogisticRegression()
-        self.case_1_logistic_regression.load_state_dict(tuple_prediction_state_dict['case_1_logistic_regression'])
-        self.case_1_logistic_regression = self.case_1_logistic_regression.to('cuda:0')
-        
-        self.case_2_logistic_regression = Case2LogisticRegression()
-        self.case_2_logistic_regression.load_state_dict(tuple_prediction_state_dict['case_2_logistic_regression'])
-        self.case_2_logistic_regression = self.case_2_logistic_regression.to('cuda:0')
-        
-        self.case_3_logistic_regression = Case3LogisticRegression()
-        self.case_3_logistic_regression.load_state_dict(tuple_prediction_state_dict['case_3_logistic_regression'])
-        self.case_3_logistic_regression = self.case_3_logistic_regression.to('cuda:0')          
-            
-    def get_calibrators(self):
-        return self.case_1_logistic_regression, self.case_2_logistic_regression, self.case_3_logistic_regression
 
-    def top3(self, subject_probs, verb_probs, object_probs, batch_p_type, scg_predictions, p_ni):
-        with torch.no_grad():
-            top3 = self.tuple_predictor.top3(subject_probs, verb_probs, object_probs, batch_p_type)
+        confidence_calibrator_state_dict = torch.load(
+            os.path.join(
+                pretrained_path,
+                'confidence-calibrator.pth'
+            ),
+            map_location='cuda:0'
+        )
+        self.confidence_calibrator = ConfidenceCalibrator().to('cuda:0')
+        self.confidence_calibrator.load_state_dict(
+            confidence_calibrator_state_dict
+        )
 
-        assert not any([any([torch.isnan(p[1]) for p in preds]) for preds in top3['top3']]), "NaNs in tuple predictor's top-3"
+        tuple_prediction_state_dict = torch.load(
+            os.path.join(
+                pretrained_path,
+                'tuple-prediction.pth'
+            ),
+            map_location='cuda:0'
+        )
 
-        for i, p in enumerate(top3['top3']):
-            new_p = []
-            for j in range(3):
-                if j < len(p):
-                    e = (p[j][0], 0.0 if torch.isnan(p[j][1]) or torch.isinf(p[j][1]) else p[j][1].item())
-                    new_svo = list(e[0])
+        self.activation_statistical_model = ActivationStatisticalModel(
+            backbone_architecture
+        ).to('cuda:0')
+        self.activation_statistical_model.load_state_dict(
+            tuple_prediction_state_dict['activation_statistical_model']
+        )
+        logit_scorer =\
+            make_logit_scorer(n_known_species_cls, n_known_activity_cls)
+        self.scorer =\
+            CompositeScorer((self.activation_statistical_model, logit_scorer))
 
-                    for k in range(3):
-                        if new_svo[k] is not None and type(new_svo[k]) == torch.Tensor:
-                            new_svo[k] = new_svo[k].item()
+        self.novelty_type_classifier = NoveltyTypeClassifier(
+            self.scorer.n_scores()
+        ).to('cuda:0')
+        self.novelty_type_classifier.load_state_dict(
+            tuple_prediction_state_dict['novelty_type_classifier']
+        )
 
-                    e = (tuple(new_svo), e[1])
-
-                else: 
-                    e = ((int(-100), int(-100), int(-100)), -100)
-
-                new_p.append(e)
-
-            top3['top3'][i] = new_p
-
-        top3_merged = self.tuple_predictor.merge_predictions(scg_predictions, top3['t67'], top3['cases'], top3['top3'], p_ni)
-
-        return top3_merged   
+    # TODO def predict(self, species_probs, activity_probs, p_type):
+    #           call self.tupe_predictor.predict()
 
     def score(self, backbone, dataset):
-        spatial_features = []
-        subject_box_features = []
-        object_box_features = []
-        verb_box_features = []
-        # whole_image_features = []
-        incident_activation_statistical_scores = []
-        
-        # TODO tensorize by case
+        # TODO tensorize / batch with dataloader
+        scores = []
+        species_probs = []
+        activity_probs = []
         with torch.no_grad():
-            for example_spatial_features, _, _, _, example_subject_images, example_object_images, example_verb_images, whole_images  in dataset:
-                spatial_feature = example_spatial_features if example_spatial_features is not None else None
-                subject_feature = backbone(example_subject_images.unsqueeze(0)).squeeze(0) if example_subject_images is not None else None
-                object_feature = backbone(example_object_images.unsqueeze(0)).squeeze(0) if example_object_images is not None else None
-                verb_feature = backbone(example_verb_images.unsqueeze(0)).squeeze(0) if example_verb_images is not None else None
-                
-                spatial_features.append(spatial_feature)
-                subject_box_features.append(subject_feature)
-                object_box_features.append(object_feature)
-                verb_box_features.append(verb_feature)    
+            for _, _, _, box_images, whole_images in dataset:
+                box_features = backbone(box_images)
+                whole_image_features =\
+                    self.activation_statistical_model.compute_features(
+                        backbone,
+                        whole_images[None]
+                    )
 
-                whole_image_features = self.activation_statistical_model.compute_features(backbone, whole_images.unsqueeze(0))
-                incident_activation_statistical_scores.append(self.activation_statistical_model.score(whole_image_features).squeeze(0))
+                cur_species_logits, cur_activity_logits =\
+                    self.classifier.predict(box_features)
+                cur_species_probs, cur_activity_probs =\
+                    self.confidence_calibrator.calibrate(
+                        cur_species_logits,
+                        cur_activity_logits
+                    )
+                species_probs.append(cur_species_probs)
+                activity_probs.append(cur_activity_probs)
+
+                cur_scores = self.scorer.score(
+                    cur_species_logits,
+                    cur_activity_logits,
+                    whole_image_features,
+                    [cur_species_logits.shape[0]]
+                )
+                scores.append(cur_scores)
+            scores = torch.cat(scores, dim=0)
 
             results = {}
-            subject_probs, subject_scores, verb_probs, verb_scores,\
-                object_probs, object_scores = self.classifier.predict_score(
-                        spatial_features,
-                        subject_box_features,
-                        verb_box_features,
-                        object_box_features
-                    )
-            results['subject_novelty_score'] = subject_scores
-            results['verb_novelty_score'] = verb_scores
-            results['object_novelty_score'] = object_scores
-            results['subject_probs'] = subject_probs
-            results['verb_probs'] = verb_probs
-            results['object_probs'] = object_probs
-            p_t4 = self.tuple_predictor.p_t4(subject_probs, verb_probs, object_probs)
-            results['p_t4'] = p_t4
+            results['scores'] = scores
+            results['species_probs'] = species_probs
+            results['activity_probs'] = activity_probs
 
-        return results, incident_activation_statistical_scores
+        return results
